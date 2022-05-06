6ac02df84bf027e20f398fef09a204ae7c3fe8c8
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-06 20:38:00 +0200

    added feature getNewUserId and getNewUserData

--- UserManager.cpp
+++ UserManager.cpp
@@ -1,1 +1,44 @@
 #include "UserManager.h"
+
+void UserManager::registerUser() {
+   User user = getNewUserData();
+
+   users.push_back(user);
+  //  plikZUzytkownikami.dopiszUzytkownikaDoPliku(user);
+
+    cout << endl << "Konto zalozono pomyslnie" << endl << endl;
+    system("pause");
+}
+User UserManager::getNewUserData() {
+    User user;
+
+    user.setId(getNewUserId());
+    string login;
+    do {
+        cout << "Podaj login: ";
+        cin >> login;
+        user.setLogin(login);
+    } while (loginExists(user.getLogin()) == true);
+
+    string password;
+    cout << "Podaj haslo: ";
+    cin >> password;
+    user.setPassword(password);
+
+    return user;
+}
+int UserManager::getNewUserId() {
+    if (users.empty() == true)
+        return 1;
+    else
+        return users.back().getId() + 1;
+}
+bool UserManager::loginExists(string login) {
+    for (int i = 0; i < users.size(); i++) {
+        if (users[i].getLogin() == login) {
+            cout << endl << "Istnieje uzytkownik o takim loginie." << endl;
+            return true;
+        }
+    }
+    return false;
+}
--- UserManager.h
+++ UserManager.h
@@ -4,10 +4,18 @@
 #include <iostream>
 #include <vector>
 
+#include "User.h"
+
 using namespace std;
 
 class UserManager{
 
-
+ vector <User> users;
+     User getNewUserData();
+     int getNewUserId();
+bool loginExists(string login);
+ public:
+     UserManager();
+     void registerUser();
 };
 #endif
