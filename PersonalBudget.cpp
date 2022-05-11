602542e03890e6e2f4f96310d7ea191b9a9ee64c
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-11 16:53:16 +0200

    created new .gitignore

---
+++ PersonalBudget.cpp
@@ -0,0 +1,56 @@
+#include "PersonalBudget.h"
+
+void PersonalBudget::registerUser() {
+    userManager.registerUser();
+}
+char PersonalBudget::selectOptionFromMainMenu() {
+    char choice;
+
+    system("cls");
+    cout << "    >>> MENU  GLOWNE <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Rejestracja" << endl;
+    cout << "2. Logowanie" << endl;
+    cout << "9. Koniec programu" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    choice = AuxiliaryMethods::loadChar();
+
+    return choice;
+}
+void PersonalBudget::loginUser() {
+    userManager.loginUser();
+}
+bool PersonalBudget::isUserLoggedIn() {
+    userManager.isUserLoggedIn();
+}
+char PersonalBudget::selectOptionFromUserMenu() {
+    char choice;
+
+    system("cls");
+    cout << " >>> MENU UZYTKOWNIKA <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Add income" << endl;
+    cout << "2. Add expense" << endl;
+    cout << "3. Display balance for current month" << endl;
+    cout << "4. Display balance for previous month" << endl;
+    cout << "5. Display balance for selected month" << endl;
+    cout << "---------------------------" << endl;
+    cout << "6. Change password" << endl;
+    cout << "7. Log out user" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    choice = AuxiliaryMethods::loadChar();
+
+    return choice;
+}
+void PersonalBudget::changePasswordLoggedInUser() {
+    userManager.changePasswordLoggedInUser();
+}
+void PersonalBudget::logOutUser() {
+    userManager.logOutUser();
+}
+void PersonalBudget::getCurrentDate() {
+    date.getCurrentDate();
+}
+
---
+++ PersonalBudget.h
@@ -0,0 +1,34 @@
+#ifndef PERSONALBUDGET_H
+#define PERSONALBUDGET_H
+
+#include <iostream>
+#include <vector>
+
+#include "UserManager.h"
+#include "AuxiliaryMethods.h"
+#include "Date.h"
+
+using namespace std;
+
+class PersonalBudget {
+    UserManager userManager;
+    Date date;
+
+
+public:
+    PersonalBudget(string fileNameWithUsers)
+        : userManager(fileNameWithUsers) {
+    };
+    void registerUser();
+    void loginUser();
+    bool isUserLoggedIn();
+    void changePasswordLoggedInUser();
+    void logOutUser();
+
+    char selectOptionFromMainMenu();
+    char selectOptionFromUserMenu();
+    void getCurrentDate();
+
+
+};
+#endif
