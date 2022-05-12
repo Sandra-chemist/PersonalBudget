b4e2ee48edd1dbd932d0019715914096f891c0bc
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-12 13:29:13 +0200

    created skeleton of function isDateCorrect

--- Date.cpp
+++ Date.cpp
@@ -13,60 +13,67 @@ int Date::getCurrentDate() {
     day = nowLocal.tm_mday;
     currentDate += day;
 
-   // cout << currentDate << endl;
-   // cout << nowLocal.tm_mday << "-" << nowLocal.tm_mon+1 << "-" << nowLocal.tm_year+1900 << endl;
-   // Sleep (5000);
+    // cout << currentDate << endl;
+    // cout << nowLocal.tm_mday << "-" << nowLocal.tm_mon+1 << "-" << nowLocal.tm_year+1900 << endl;
+    // Sleep (5000);
     return currentDate;
 }
-string Date::convertCurrentDateFromIntToString(int currentDate)
-{
+string Date::convertCurrentDateFromIntToString(int currentDate) {
     string dateAsString;
     dateAsString = AuxiliaryMethods::convertIntToString(currentDate);
     Sleep (5000);
     return dateAsString;
 }
-string Date::writeOtherDateThanCurrent()
-{
-     string otherDate;
-     cout << "Provide date in format YYYY-MM-DD: ";
-     otherDate = AuxiliaryMethods::loadLine();
-     cout << otherDate << endl;
+string Date::writeOtherDateThanCurrent() {
+    string otherDate;
+    cout << "Provide date in format YYYY-MM-DD: ";
+    otherDate = AuxiliaryMethods::loadLine();
 
-     int lengthOfOtherDate = otherDate.length();
-     for (int i = 0; i < lengthOfOtherDate - 6; i ++)
-     {
-           string year;
-           year = otherDate[i];
-           cout << year;
-     }
-     cout << endl;
-      for (int i = 5; i < lengthOfOtherDate - 3; i ++)
-     {
-           string month;
-           month = otherDate[i];
-           cout << month;
-     }
-     cout << endl;
-      for (int i = 8; i < lengthOfOtherDate; i ++)
-     {
-           string day;
-           day = otherDate[i];
-           cout << day;
-     }
-
- Sleep (2000);
+    if (isDateCorrect(otherDate) == true)
+        {
+         cout << otherDate << endl;
+         Sleep(5000);
+        }
 
     /* string otherDateWithoutDash;
      otherDateWithoutDash = AuxiliaryMethods::removeDashFromDate(otherDate);
      cout << otherDateWithoutDash << endl;
      Sleep (2000);*/
-     return otherDate;
+    return otherDate;
 }
-bool Date::isDateCorrect(string otherDate)
-{
-    string otherDateWithoutDash;
-    otherDateWithoutDash = AuxiliaryMethods::removeDashFromDate(otherDate);
+bool Date::isDateCorrect(string otherDate) {
 
+    int lengthOfOtherDate = otherDate.length();
+    for (int i = 0; i < lengthOfOtherDate - 6; i ++) {
+        string year;
+        year = otherDate[i];
+        cout << year;
+    }
+    if (year >= 2000)
+    {
+        cout << year;
+        return true;
+    }
+    cout << endl;
+    for (int i = 5; i < lengthOfOtherDate - 3; i ++) {
+        string month;
+        month = otherDate[i];
+        cout << month;
+    }
+    if (month > 0 && month < 13)
+    {
+        return true;
+    }
+    cout << endl;
+    for (int i = 8; i < lengthOfOtherDate; i ++) {
+        string day;
+        day = otherDate[i];
+        cout << day;
+    }
+    if (day > 0 && day < 32)
+    {
+        return true;
+    }
 }
 
 
