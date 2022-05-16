1733fc94e2eb75140d4a2caf447969189ba73cce
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-16 22:20:05 +0200

    added getters and setters in Expense class

--- Expense.h
+++ Expense.h
@@ -8,5 +8,24 @@ using namespace std;
 
 class Expense {
 
+    int expenseId;
+    int userId;
+    string date;
+    string item;
+    float amount;
+
+public:
+    void setExpenseId(int newExpenseId);
+    void setUserId(int newUserId);
+    void setDate(string newDate);
+    void setItem(string newItem);
+    void setAmount(float newAmount);
+
+    int getExpenseId();
+    int getUserId();
+    string getDate();
+    string getItem();
+    float getAmount();
+
 };
 #endif
