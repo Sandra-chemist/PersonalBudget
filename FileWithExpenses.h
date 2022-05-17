8e227f8586fedc7833ff0f6842e0e658866bd754
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-17 12:13:28 +0200

    added class Expense and FileWithExpenses

---
+++ FileWithExpenses.h
@@ -0,0 +1,26 @@
+#ifndef FILEWITEXPENSES_H
+#define FILEWITHEXPENSES_H
+
+#include <iostream>
+#include <vector>
+
+#include "Expense.h"
+#include "Markup.h"
+#include "XmlFile.h"
+#include "AuxiliaryMethods.h"
+
+using namespace std;
+
+class FileWithExpenses : public XmlFile {
+
+    string fileNameWithExpenses;
+    vector <Expense> expenses;
+    int loggedInUserId;
+
+public:
+    FileWithExpenses(string fileNameWithExpenses) : XmlFile(fileNameWithExpenses) {};
+
+    void addExpenseToFile(Expense expense);
+    vector <Expense> loadExpensesFromFile(int loggedInUserId);
+};
+#endif
