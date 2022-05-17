b2326cbcbcc24ba9900eb58ed5099306373d36e4
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-05-17 13:45:42 +0200

    added new class: ExpenseManager

---
+++ ExpenseManager.h
@@ -0,0 +1,20 @@
+#ifndef EXPENSEMANAGER_H
+#define EXPENSEMANAGER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <sstream>
+
+#include "Expense.h"
+#include "Date.h"
+#include "FileWithExpenses.h"
+#include "AuxiliaryMethods.h"
+
+using namespace std;
+
+class ExpenseManager {
+
+
+};
+#endif
